2544ff0c0ab254a4e5a3a4a5ae99741c9e6e0982
mohammad moradi <mohamadhoseinmoradi97@gmail.com>
2022-01-24 18:30:17 +0100

    changes

---
+++ tests/test_method.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import sys
+sys.path.insert(1, '/home/mdmd/PYTHON/flask-app/')
+
+from application.application import myapp
+
+def method_test():
+    test_app = myapp
+    with test_app.test_client() as test_client:
+        response = test_client.get('/tree')
+    #assert response.get_data == "asghaaar"
+    assert response.status_code == 200
--- tests/unit/test_method.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(1, '/home/mdmd/PYTHON/flask-app/')
-
-from application.application import myapp
-
-print (myapp())
