59b882b8b59da9b08a70ccef69c956c28f3978d9
Mohamad Hosein Moradi <72094509+mohamadhoseinmoradi@users.noreply.github.com>
2022-01-28 17:58:51 +0100

    Update test_method.py

--- application/tests/test_method.py
+++ application/tests/test_method.py
@@ -1,13 +1,6 @@
-from flask import Flask
-import sys
-sys.path.insert(1, '/home/mdmd/PYTHON/flask-app/')
+from wsgi import app
 
-from application.application import myapp
-
-def method_test():
-    test_app = myapp
-    with test_app.test_client() as test_client:
-        response = test_client.get('/tree')
-    #assert response.get_data == "asghaaar"
+def test_app_route_response():
+    response = app.test_client().get('/tree')
     assert response.status_code == 200
 
