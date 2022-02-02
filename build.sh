4dfdede3a338b15a650afbaab6a060dbbefce43b
Mohamad Hosein Moradi <72094509+mohamadhoseinmoradi@users.noreply.github.com>
2022-02-02 16:30:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,4 +10,6 @@ if [[ $# -lt 2 ]] ; then
     exit 1
 fi
 
-docker build -f docker/Dockerfile -t  $image_name:$image_tag .
+docker build -f docker/Dockerfile --tag  $image_name:$image_tag .
+docker tag app:0.1 artifactory.jfrog.io/app/app:0.1
+docker push artifactory.jfrog.io/app/app:0.1
