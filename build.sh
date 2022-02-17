2f74437fb3e15c6a9fc8e29eb2ba4548ca6d858b
mohammad moradi <mohamadhoseinmoradi97@gmail.com>
2022-02-17 19:19:26 +0100

    sidecar fully implemented

--- build.sh
+++ build.sh
@@ -3,13 +3,22 @@ set -e
 
 image_name=$1
 image_tag=$2
+release_name=$3
 
-if [[ $# -lt 2 ]] ; then
-    echo "you have to pass at least two arguments!"
-    echo "usage: docker-build.sh <image_name>:<image_tag>"
+if [[ $# -lt 3 ]] ; then
+    echo "you have to pass at least three arguments!"
+    echo "usage: docker-build.sh <image_name> <image_tag> <release_name>"
     exit 1
 fi
 
+echo "building the artifact ..."
 docker build --file Dockerfile --tag  $image_name:$image_tag .
-#docker tag app:0.1 artifactory.jfrog.io/app/app:0.1
-#docker push artifactory.jfrog.io/app/app:0.1
+echo "Done"
+echo "tagging and pushing the artifact ..."
+docker tag $image_name:$image_tag mdmddockergft/$image_name:$image_tag
+docker push mdmddockergft/$image_name:$image_tag
+echo "Done"
+
+echo "deploying application - running Helm ..."
+helm install $release_name helm/
+echo "Done"
